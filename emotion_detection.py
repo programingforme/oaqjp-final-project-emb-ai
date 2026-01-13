c7dea86d1af22dd7f24c7594c9db023b9f5fae59
programingforme <programingforme@gmail.com>
2026-01-13 05:44:00 -0500

    implement emotion_detector function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,9 @@
-import json
 import requests
 
-def emotion_detector():
-    
+def emotion_detector(text_to_analyse):
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_text = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url, headers=headers, json=input_text, timeout=10)
+    return response.text
