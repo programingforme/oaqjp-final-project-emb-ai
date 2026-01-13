bd9176a5a78e1effb20cdc27ba51fbe43b5dbb66
programingforme <programingforme@gmail.com>
2026-01-13 10:14:46 -0500

    update server.py to be pylint compliant

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+''' This is the final project
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +7,8 @@ app = Flask('Emotion_Detection')
 
 @app.route('/emotionDetector')
 def emotion_detection():
+    ''' The emotion_detection endpoint
+    '''
     text_to_analyse = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyse)
@@ -17,7 +21,9 @@ def emotion_detection():
     dominant_emotion = response['dominant_emotion']
 
     if dominant_emotion:
-        return_response = f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."    
+        return_response = f"For the given statement, the system response is 'anger': {anger}, " \
+        f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. " \
+        f"The dominant emotion is {dominant_emotion}."
     else:
         return_response = "Invalid text! Please try again!"
 
@@ -26,6 +32,8 @@ def emotion_detection():
 
 @app.route("/")
 def index_page():
+    ''' The index_page endpoint
+    '''
     return render_template("index.html")
 
 if __name__ == "__main__":
