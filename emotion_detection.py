97a94b6d773427fc6858098a6cc0f8a626ef5206
programingforme <programingforme@gmail.com>
2026-01-13 06:34:49 -0500

    update emotion_detector function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyse):
@@ -6,4 +7,24 @@ def emotion_detector(text_to_analyse):
     input_text = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, headers=headers, json=input_text, timeout=10)
-    return response.text
+    json_response = json.loads(response.text)
+
+    anger = json_response['emotionPredictions'][0]['emotion']['anger']
+    disgust = json_response['emotionPredictions'][0]['emotion']['disgust']
+    fear = json_response['emotionPredictions'][0]['emotion']['fear']
+    joy = json_response['emotionPredictions'][0]['emotion']['joy']
+    sadness = json_response['emotionPredictions'][0]['emotion']['sadness']
+    
+    result =  {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+
+    dominant_emotion = max(result, key=result.get)
+    result['dominant_emotion'] = dominant_emotion
+
+    return result
+    
